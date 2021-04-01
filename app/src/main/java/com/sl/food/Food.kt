6ec6ed1e36d3e161ed9fe7f204f99ddb46a35295
sherlynlobo <sherlynlobo26@gmail.com>
2021-04-01 16:07:54 -0400

    Grid View

---
+++ app/src/main/java/com/sl/food/Food.kt
@@ -0,0 +1,13 @@
+package com.sl.food
+
+class Food {
+    var name: String? = null
+    var des: String? = null
+    var image: Int? = null
+
+    constructor(name: String?, des: String?, image: Int?) {
+        this.name = name
+        this.des = des
+        this.image = image
+    }
+}
